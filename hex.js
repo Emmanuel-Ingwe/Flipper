4ccd2c95e59b9b52dc5aadf8df755f191e90e630
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-10 05:43:56 +0200

    ++

--- hex.js
+++ hex.js
@@ -2,6 +2,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const h4 = document.querySelector('.h4');
 
 btn.addEventListener('click', () => {
   let hexColor = '#';
@@ -10,6 +11,7 @@ btn.addEventListener('click', () => {
   }
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
+  document.querySelector('.h4').style.color = hexColor;
 });
 
 function getRandomColor() {
